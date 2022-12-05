095ab5ff22e57e1b3e207ec917b152fda5dd48df
Daksh Bhatia <daksh001b@gmail.com>
2022-12-05 15:10:20 +0530

    A6 complete

---
+++ Assignment_6/q2.java
@@ -0,0 +1,31 @@
+public class Question_2 {
+    public static void addition(int a, int b) {
+        try {
+            if (a > 0 && b > 0) {
+                System.out.println("Addition is : " + (a + b));
+            } else {
+                throw new ArithmeticException("Negative input cannot be taken");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void subtract(double a, double b) {
+        try {
+            if (a > 0 && b > 0) {
+                System.out.println(a - b);
+            } else {
+                throw new ArithmeticException("Negative input cannot be taken");
+            }
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        addition(-4, 8);
+        subtract(3, -1);
+
+    }
+}
---
+++ Assignment_6/q3.java
@@ -0,0 +1,11 @@
+package Question_3;
+
+public class Calculator {
+    Object first;
+    Object second;
+
+    public Calculator(Object a, Object b) {
+        this.first = a;
+        this.second = b;
+    }
+}
